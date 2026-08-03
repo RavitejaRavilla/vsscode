145932988aaa2914c82a1456b0a11c15d25afe94
Raviteja Ravilla <ravillaraviteja342@gmail.com>
2026-08-03 11:33:52 +0530

    playwright tet vscode

---
+++ nenu/variable.js/primevariable.js
@@ -0,0 +1,103 @@
+var nume=100
+var prime =true
+
+for ( i=2; i<nume; i++) {
+    if (nume % i == 0) {
+        prime = false;
+        break;
+    }
+}
+    if(prime){
+        console.log(nume + " is a prime number");
+    }else{
+        console.log(nume + " is not a prime number");
+    }
+
+    var ne=100
+    for ( ne=2; i<ne; i++) {
+        var prime=true;
+
+    for( i=2; i<ne; i++) {
+        if(ne % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+}
+    if(prime){
+        console.log(ne + " is a prime number");
+    }else{
+        console.log(ne + " is not a prime number");
+    }
+
+
+    let n = 100;
+
+for (let nume = 2; nume <= n; nume++) {
+    let prime = true;
+
+    for (let i = 2; i < nume; i++) {
+        if (nume % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime == true) {
+        console.log(nume);
+    }
+}
+
+for(let nme=1;nme<10;nme++){
+
+for(let i=1; i<10; i++){
+    if(i+nme==10){
+        console.log(i,nme);
+    }
+}
+}
+
+for (let row1=1;row1<=3;row1++){
+    console.log(row1)
+    for(let seat=1;seat<=4;seat++){
+        console.log("row" + row1 + ", seat" + seat);
+}
+}
+
+var nl=1
+var result=""
+while(nl<=30){
+    result= result + " " +nl
+nl +=1
+// result=("num=" + nl)
+
+}
+
+console.log(result)
+
+
+//var ne=3
+    for ( ne=2; ne<3; ne++) {
+        var prime=true;
+
+    for( i=2; i<ne; i++) {
+        if(ne % i == 0) {
+            prime = false;
+            break;
+        }
+    }
+}
+    if(prime){
+        console.log(ne + " is a prime number");
+    }else{
+        console.log(ne + " is not a prime number");
+    }
+varrt=1
+while(true){
+console.log(varrt)
+varrt +=1
+if(varrt==50){
+break
+}
+    
+}
